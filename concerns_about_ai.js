a132fe3f7b1bfa2d5145d95cf33ab989ff780f2e
Connor Pummill <73847860+PummCon@users.noreply.github.com>
2023-11-27 22:04:04 -0500

    Update concerns_about_ai.js

--- concerns_about_ai.js
+++ concerns_about_ai.js
@@ -1,7 +1,17 @@
 // concerns_about_ai.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.body.style.opacity = 1; // Fade in the body content
+    // Get the current date and time
+    const now = new Date();
+    const dateString = now.toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    const timeString = now.toLocaleTimeString("en-US");
+
+    // Display the current date and time
+    const currentDate = document.querySelector('time');
+    if (currentDate) {
+        currentDate.textContent = dateString + ' ' + timeString;
+        currentDate.style.fontWeight = 'bold';
+    }
 
     // Any other JavaScript functionalities can be added here
 });
