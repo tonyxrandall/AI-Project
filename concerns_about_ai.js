6e4a022056d595d259eb5d059b3744143383c67a
Connor Pummill <73847860+PummCon@users.noreply.github.com>
2023-11-27 21:57:21 -0500

    Update concerns_about_ai.js

--- concerns_about_ai.js
+++ concerns_about_ai.js
@@ -1,17 +1,7 @@
 // concerns_about_ai.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    const headerImage = document.getElementById('header-image');
-    if (headerImage) {
-        headerImage.style.opacity = 1;
-    }
+    document.body.style.opacity = 1; // Fade in the body content
 
-    // Scroll down after the image has faded in
-    setTimeout(function() {
-        // Adjust the '800' to the pixel value you want to scroll to, or use a dynamic value
-        window.scrollTo({
-            top: 800, 
-            behavior: 'smooth' 
-        });
-    }, 6000); // 2000 milliseconds (2 seconds) to match the fade-in duration
+    // Any other JavaScript functionalities can be added here
 });
