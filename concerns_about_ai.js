79995e14054bd88874004a976b3790d592a7f55b
Connor Pummill <73847860+PummCon@users.noreply.github.com>
2023-11-27 20:49:05 -0500

    Create concerns_about_ai.js

---
+++ concerns_about_ai.js
@@ -0,0 +1,7 @@
+// concerns_about_ai.js
+
+// Example of a simple JavaScript functionality
+document.addEventListener('DOMContentLoaded', function() {
+    const currentDate = document.querySelector('time');
+    currentDate.style.fontWeight = 'bold';
+});
